ef1c082706fc9f7480ee80816f84bf7dfcd403c7
Chun-Hung Tseng <henrybear327@gmail.com>
2024-02-07 20:53:10 +0100

    Don't print traces when NULL ptr is freed

--- src/mpool_trc.c
+++ src/mpool_trc.c
@@ -50,9 +50,47 @@ void *calloc_with_trace(size_t __count, size_t __size)
     return ptr;
 }
 
+/**
+ * Reference:
+ * https://stackoverflow.com/questions/4636456/how-to-get-a-stack-trace-for-c-using-gcc-with-line-number-information
+ */
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void print_trace()
+{
+    char pid_buf[30];
+    sprintf(pid_buf, "%d", getpid());
+    char name_buf[512];
+    name_buf[readlink("/proc/self/exe", name_buf, 511)] = 0;
+    prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+    int child_pid = fork();
+    if (!child_pid) {
+        dup2(2, 1);  // redirect output to stderr - edit: unnecessary?
+        execl("/usr/bin/gdb", "gdb", "--batch", "-n", "-ex", "thread", "-ex",
+              "bt", name_buf, pid_buf, NULL);
+        abort(); /* If gdb failed to start */
+    } else {
+        waitpid(child_pid, NULL, 0);
+    }
+}
+/* =============== */
+
 void free_with_trace(void *ptr)
 {
     // fprintf(stderr, "free_with_trace()\n");
+
+    // if the ptr is NULL, there is nothing to free
+    if (ptr == NULL) {
+        /**
+         * Potentially called from 
+         * - block_map_clear (rv=0xaaaae256f2c0) at src/riscv.c:58
+         */
+        // print_trace();
+        
+        return;
+    }
 
     // begin time
     clock_t start_t = clock();
